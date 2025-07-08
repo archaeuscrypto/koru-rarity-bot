c4229da3f8f14738904b07800fed7932351c3801
Noah Richard <1145392@palmbeach.k12.fl.us>
2025-07-08 12:01:51 -0400

    v1.01

--- bot.py
+++ bot.py
@@ -7,7 +7,9 @@ with open('rarity-ranking.json') as f:
     rarity_data = json.load(f)
 
 intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 @bot.event
 async def on_ready():
